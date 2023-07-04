5dc1f042f83375a8b601d01bc63b45b846bd23fc
serchonodera117 <sergio.lobo117@gmail.com>
2023-07-04 15:38:06 -0600

    animations, scenes and scripts

---
+++ Assets/Scripts/camera_controll.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controll : MonoBehaviour
+{
+    public Vector3 offset; //distancia entre camara y jugador
+    public Transform player;
+    [Range(0, 1)] public float smooth;
+
+    public bool mirandoBragas = false;
+    public playerMovement options;
+    public bool rotacionActive = true;
+    public float velocidadCamara = 1f;
+
+    public bool lookAtPlayer = false;
+
+    private short limiteMin = 2;
+    private short limiteMax = 100;
+    private float myTimer = 0;
+    private bool activarTimer = false;
+    private Camera myCamara;
+    void Start()
+    {
+        offset = transform.position - player.position;
+        myCamara = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        controlCamara();
+        if (activarTimer) myTimer += Time.deltaTime;
+        Debug.Log("segundos = "+ myTimer);
+    }
+    void controlCamara()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && myCamara.fieldOfView > limiteMin)
+            myCamara.fieldOfView--;
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && myCamara.fieldOfView < limiteMax)
+            myCamara.fieldOfView++;
+
+        if (rotacionActive)
+        {
+            Quaternion angulo = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * velocidadCamara, Vector3.up) * Quaternion.AngleAxis(-Input.GetAxis("Mouse Y") * velocidadCamara, Vector3.right);
+            offset = angulo * offset;
+        }
+
+        Vector3 newPos = player.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smooth);
+
+        if (lookAtPlayer || rotacionActive)
+        {
+            transform.LookAt(player);
+        }
+
+        if (offset.y < -1.5)
+        {
+            myCamara.fieldOfView = 13;
+            mirandoBragas = true;
+            options.looking = true;
+            activarTimer = true;
+        }
+        else if (offset.y > -1.5 && mirandoBragas)
+        {
+            myCamara.fieldOfView = 46;
+            mirandoBragas = false;
+            options.looking = false;
+            resetTimer();
+        }else
+            resetTimer();
+
+        if (myTimer > 1.5)
+       {
+            offset.y = 3;
+            myCamara.fieldOfView = 46;
+            Debug.Log("¡No mires ahí, Biejo cochino!");
+            resetTimer();
+        }
+    }
+    void resetTimer(){
+        myTimer = 0;
+        activarTimer = false;
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float Speed = 1f;
+    public bool grounded;
+    public bool caminando;
+    private bool dance = false;
+    private bool up_down = true;
+
+    public Animator animaciones;
+
+    public float cameraSpeed = 1f;
+    public float salto = 1f;
+    private Rigidbody fisicas;
+
+    public bool looking; 
+   
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        fisicas = GetComponent<Rigidbody>();
+        animaciones = GetComponent<Animator>();
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        animaciones.SetBool("Grounded", grounded);
+        animaciones.SetBool("Caminar", caminando);
+        animaciones.SetBool("Dancing", dance);
+        animaciones.SetBool("Look_down", looking);
+        Debug.Log("mirando = "+looking);
+        
+   
+        movimiento();
+        rotacionCamara();
+
+       
+    }
+
+
+    //- - - - Movimiento del jugador y movimiento de la camara
+    void movimiento(){
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+    
+        if (horizontal != 0 || vertical != 0)
+        {
+            vertical = RotacionMovimientoPlayer(horizontal, vertical); 
+         
+            Vector3 direccion = transform.forward * vertical + transform.right * horizontal; 
+            fisicas.MovePosition(transform.position + direccion * Speed);
+            caminando = true;
+            Debug.Log("2.- Horizontal : "+horizontal+ " Vertical : "+ vertical);
+        }
+        else caminando = false;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fisicas.AddForce(new Vector3(0, salto, 0), ForceMode.Impulse);
+            grounded = false;
+        }
+        else grounded = true;
+
+        if (Input.GetKey(KeyCode.E)) dance = true;
+        else dance = false;
+
+    }
+    float RotacionMovimientoPlayer(float horizontal, float vertical)
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (up_down == true) transform.Rotate(new Vector3(0, 180, 0));
+            up_down = false;
+            return 1;
+        }else if(Input.GetKeyDown(KeyCode.W)){
+            if (up_down == false) transform.Rotate(new Vector3(0, 180, 0));
+            up_down = true;
+            return 1;
+        }else return 1;
+     }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Suelo") grounded = true; Debug.Log("tocando suelo");
+    }
+
+   
+
+    void rotacionCamara(){
+        float rotationX = Input.GetAxis("Mouse X");
+        transform.Rotate(new Vector3(0, rotationX * Time.deltaTime * cameraSpeed, 0));
+    }
+
+}
